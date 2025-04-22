818756391b28ef35173ebe3e189c99ea9d71ecb9
Emma <you@example.com>
2025-04-22 10:05:57 +0200

    Installed EF and first migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using BookLibrary.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); // Ändra om du använder en annan databas
 
 var app = builder.Build();
 
