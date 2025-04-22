2ce835c5380e55c52608fa410d73ec566cf2dab4
Emma <you@example.com>
2025-04-22 09:38:45 +0200

    Added models for books and genre

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public bool IsRead { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+}
