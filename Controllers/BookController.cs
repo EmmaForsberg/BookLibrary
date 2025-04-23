a342f6a7566eb5e3480de41942e0cf7ab11423a2
Emma <you@example.com>
2025-04-23 09:54:46 +0200

    Created a controller for Books and a view viewing seeded books

---
+++ Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookLibrary.Data;
+
+namespace BookLibrary.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        // Konstruktorn injicerar AppDbContext
+        public BookController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _db.Books.ToList();
+            return View(list);
+        }
+    }
+}
